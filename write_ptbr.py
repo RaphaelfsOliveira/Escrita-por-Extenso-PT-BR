2dc74fa17b434d444235d3bab197651706ecf5bb
Raphael Oliveira <raphael.oliveira@brilliantmachine.com.br>
2019-09-28 19:55:58 -0300

    - adicionando mil/milhão

--- write_ptbr.py
+++ write_ptbr.py
@@ -25,7 +25,7 @@ class WriteOutPTBR(object):
         }
 
         self.magnitude = {
-            'mil', 'milhão', 'milhões'
+            0: 'mil', 1: 'milhão', 2: 'milhões'
         }
 
     def make_unity(self, unity, ten=0):
@@ -43,7 +43,6 @@ class WriteOutPTBR(object):
 
     def make_unit_and_ten(self, numbers):
         if len(numbers) > 1 and int(numbers[0]) not in [0, 1] and int(numbers[1]) != 0:
-
             return '{} e {}'.format(self.make_ten(numbers[0], numbers[1]),
                                     self.make_unity(ten=numbers[0], unity=numbers[1]))
         elif len(numbers) == 1:
@@ -59,6 +58,9 @@ class WriteOutPTBR(object):
         if length == 3:
             if int(integers) == CEM:
                 return '{}'.format(self.hundred[CEM])
+            if int(integers[1]) == 0 and int(integers[2]) == 0:
+                return '{} {}'.format(self.make_hundred(integers[0]),
+                                     self.make_unit_and_ten([integers[1], integers[2]]))
 
             return '{} e {}'.format(self.make_hundred(integers[0]),
                                     self.make_unit_and_ten([integers[1], integers[2]]))
@@ -67,14 +69,49 @@ class WriteOutPTBR(object):
         elif length == 1:
             return '{}'.format(self.make_unity(unity=integers))
 
+    def add_thousand(self, thousand):
+        if int(thousand):
+            return '{} {} '.format(self.make_unit_ten_hundred(thousand),
+                                   self.magnitude[0])
+        return ''
+
+    def add_million(self, million, mag):
+        magnitude = self.magnitude[2]
+        if mag == 1 and len(million) == 1: magnitude = self.magnitude[1]
+
+        return '{} {}'.format(self.make_unit_ten_hundred(million),
+                              magnitude)
+
+    def add_magnitude(self, integers):
+        length = int(len(integers))
+        
+        if length <= 3:
+            return '{}'.format(self.make_unit_ten_hundred(integers))
+
+        if length <= 6:
+            hundred = integers[length - 3: length]
+            thousand = integers[0:length - 3]
+
+            return '{} {}'.format(self.add_thousand(thousand),
+                                  self.make_unit_ten_hundred(hundred))
+
+        if length <= 9:
+            hundred = integers[length - 3: length]
+            thousand = integers[length - 6:length - 3]
+            million = integers[0:length - 6]
+
+            return '{} {} {}'.format(self.add_million(million, int(integers[length - 7])),
+                                     self.add_thousand(thousand),
+                                     self.make_unit_ten_hundred(hundred))
+
     def written_in_full(self, number):
         edit_number = str(number).split('.')
 
         integers = edit_number[0]
         decimals = edit_number[1]
 
-        return '{}, {}'.format(self.make_unit_ten_hundred(integers),
-                               self.make_unit_and_ten(decimals))
+        return '{}, {}'.format(self.add_magnitude(integers),
+                               self.make_unit_and_ten(decimals)).replace('  ', ' ')
 
 if __name__ == '__main__':
     from random import randrange, uniform
@@ -82,11 +119,15 @@ if __name__ == '__main__':
     w = WriteOutPTBR()
 
     numbers = ['01', '02', '03', '04', '05', '06', '07', '08', '09']
+    numbers = [700009.02, 800009.02, 900009.02]
+    numbers = range(0, 50)
 
-    for n in range(0, 50):
-        number = round(uniform(2.5, 399.99), 2)
+    for number in numbers:
+        number = round(uniform(0, 91955600.99), 2)
+        # number = 1000000.45
+        # number = 11690857.0
 
-        print_out = '{:>50} {:<} {:<}'.format(w.written_in_full(number),
+        print_out = '{:>120} {:<} {:<}'.format(w.written_in_full(number),
                                               ' --> ',
                                               number)
         print(print_out)
